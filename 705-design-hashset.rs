f05e670b93b9b622211600a61608f6be90d13b41
memorycancel <memorycancel@gmail.com>
2022-08-29 11:44:07 +0800

    Remove blanks

--- 705-design-hashset.rs
+++ 705-design-hashset.rs
@@ -13,15 +13,15 @@ impl MyHashSet {
     fn new() -> Self {
         MyHashSet {set: HashSet::new()}
     }
-    
+
     fn add(&mut self, key: i32) {
         self.set.insert(key);
     }
-    
+
     fn remove(&mut self, key: i32) {
         self.set.remove(&key);
     }
-    
+
     fn contains(&self, key: i32) -> bool {
         self.set.contains(&key)
     }
--- 706-design-hashmap.rs
+++ 706-design-hashmap.rs
@@ -15,12 +15,12 @@ impl MyHashMap {
     fn new() -> Self {
         MyHashMap{map: HashMap::new()}
     }
-    
+
     /** value will always be non-negative. */
     fn put(&mut self, key: i32, value: i32) {
         self.map.insert(key, value);
     }
-    
+
     /** Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key */
     fn get(&self, key: i32) -> i32 {
         if self.map.get(&key).is_some() {
@@ -29,7 +29,7 @@ impl MyHashMap {
             -1
         }
     }
-    
+
     /** Removes the mapping of the specified value key if this map contains a mapping for the key */
     fn remove(&mut self, key: i32) {
         self.map.remove(&key);
